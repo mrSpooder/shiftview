e7f021729a28608766340a4663318c4b3eb68368
mrSpooder <74469650+mrSpooder@users.noreply.github.com>
2021-02-24 15:55:37 -0300

    Add files via upload

---
+++ shiftview.c
@@ -0,0 +1,27 @@
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+void
+shiftview(const Arg *arg, const int n) {
+	Arg shifted;
+
+	if(arg->i > 0) // left circular shift
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+	else // right circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	if(n == 0) {
+		view(&shifted);
+	}
+
+	else {
+		tag(&shifted);
+		view(&shifted);
+	}
+
+}
