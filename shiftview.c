2a4e98f22b031d103f97d38c72f407fb8ba98216
mrSpooder <74469650+mrSpooder@users.noreply.github.com>
2021-02-24 15:56:25 -0300

    Add files via upload

--- shiftview.c
+++ shiftview.c
@@ -3,25 +3,34 @@
  * @param: "arg->i" stores the number of tags to shift right (positive value)
  *          or left (negative value)
  */
+
+#define SHIFT_LEFT              1 << 0
+#define SHIFT_RIGHT             1 << 1
+#define SHIFT_TAG		1 << 2
+#define SHIFT_VIEW		1 << 3
+
+
+{ MODKEY,           XK_o,  shiftview,      { .ui = SHIFT_RIGHT | SHIFT_VIEW } },
+{ MODKEY,           XK_i,   shiftview,      { .ui = SHIFT_LEFT | SHIFT_VIEW} },
+{ MODKEY|ShiftMask, XK_o,  shiftview,      { .ui = SHIFT_RIGHT | SHIFT_TAG | SHIFT_VIEW} },
+{ MODKEY|ShiftMask, XK_i,   shiftview,      { .ui = SHIFT_LEFT | SHIFT_TAG | SHIFT_VIEW} },
+
 void
-shiftview(const Arg *arg, const int n) {
+shiftview(const Arg *arg) {
 	Arg shifted;
+	unsigned int a = arg->ui;
 
-	if(arg->i > 0) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+	if(a & SHIFT_LEFT) // left circular shift
+		shifted.ui = (selmon->tagset[selmon->seltags] << 1)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - 1));
 
-	else // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+	else if(a & SHIFT_RIGHT) // right circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] >> 1
+		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) - 1);
 
-	if(n == 0) {
-		view(&shifted);
-	}
-
-	else {
+	if(a & SHIFT_TAG) 
 		tag(&shifted);
+	
+	if(a & SHIFT_VIEW)
 		view(&shifted);
-	}
-
 }
