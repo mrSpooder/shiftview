33e97e7c70567500c07ee5cf9299905fab0466d1
mrSpooder <joaodear2007@gmail.com>
2021-02-24 17:32:27 -0300

    reversed shifts to correct direction

--- shiftview.c
+++ shiftview.c
@@ -1,19 +1,21 @@
-/** Function to shift the current view to the left/right
- *
- * @param: "arg->i" stores the number of tags to shift right (positive value)
- *          or left (negative value)
- */
+//////////////////////////////////////////////////////////////////////
+/* moves clients and or focus circularly right or left through tags */
+//////////////////////////////////////////////////////////////////////
 
-#define SHIFT_LEFT              1 << 0
+// add the following 5 lines anywhere in config.def.h
+
+/*#define SHIFT_LEFT              1 << 0
 #define SHIFT_RIGHT             1 << 1
 #define SHIFT_TAG		1 << 2
 #define SHIFT_VIEW		1 << 3
+#include "shiftview.c"
 
+// add the following 4 lines to static Key keys[] = {} in config.def.h
 
 { MODKEY,           XK_o,  shiftview,      { .ui = SHIFT_RIGHT | SHIFT_VIEW } },
 { MODKEY,           XK_i,   shiftview,      { .ui = SHIFT_LEFT | SHIFT_VIEW} },
 { MODKEY|ShiftMask, XK_o,  shiftview,      { .ui = SHIFT_RIGHT | SHIFT_TAG | SHIFT_VIEW} },
-{ MODKEY|ShiftMask, XK_i,   shiftview,      { .ui = SHIFT_LEFT | SHIFT_TAG | SHIFT_VIEW} },
+{ MODKEY|ShiftMask, XK_i,   shiftview,      { .ui = SHIFT_LEFT | SHIFT_TAG | SHIFT_VIEW} },*/
 
 void
 shiftview(const Arg *arg) {
@@ -21,12 +23,12 @@ shiftview(const Arg *arg) {
 	unsigned int a = arg->ui;
 
 	if(a & SHIFT_LEFT) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << 1)
-		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - 1));
+		shifted.ui = (selmon->tagset[selmon->seltags] >> 1)
+		   | (selmon->tagset[selmon->seltags] << (LENGTH(tags) - 1));
 
 	else if(a & SHIFT_RIGHT) // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> 1
-		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) - 1);
+		shifted.ui = (selmon->tagset[selmon->seltags] << 1)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - 1));
 
 	if(a & SHIFT_TAG) 
 		tag(&shifted);
